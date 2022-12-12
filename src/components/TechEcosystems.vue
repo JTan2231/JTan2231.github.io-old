f3451572525f34f8e6c3f58e1229e95d2cf1a99c
joey <j.tan2231@gmail.com>
2022-12-11 19:47:46 -0500

    Technological Ecosystems essay

---
+++ src/components/TechEcosystems.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="description">
+        <p>
+        <u>Preface:</u> I've been watching a lot of nature documentaries, so the concepts of evolution and ecosystems have been heavy on my mind. This, combined with recent AI developments and reading the Network State, has resulted in below.
+        </p>
+        <p>
+        Humans are tool-using animals. Various phases of history have been started by different innovations in various technologies, which in turn open the door to entirely new behaviors on a mass scale. Writing allowed the dissemination and long-term recording of knowledge at the precision of the writer’s preference. Metalworking paved the way for new and improved forms of warfare and machinery to automate physical tasks in the world. No longer did humanity have to manually perform certain forms of labor or pass down different kinds of knowledge to their kids; these new tools took care of that. But these ecosystems are not perfect. Instead, they’re constantly developing in response to each other. Evolving to further meet the needs of a changing environment.
+        </p>
+        <p>
+        The exploration of these toolings became its own ecosystem that existed through humanity’s continued usage and research. Special gardens of academic inquiry, known as universities, cropped up in order to cultivate these sacred crops. It was, after all, the mighty hammer and chisel which allowed man to so easily chisel the world to his liking. Nurturing these fields of study in obsessive depths revealed new fields of study, ripe for the picking. But rather than being entirely new subject matters, these were often variations on one another, similar to how a tangerine is a variation on an orange.
+        </p>
+        <p>
+        Engineering can be considered the study of tooling, and is probably the best example of this concept of variation. All disciplines can be considered the usage of tools to manipulate the world into achieving some desired end product. Mechanical engineering manipulates the Newtonian forces of matter to move and mold the world around us; chemical engineering manipulates the atomic combinations of matter to convert the world around us from one form to another; electrical engineering manipulates the electromagnetic tensions and subatomic properties of materials to manipulate the world in a more abstract manner. What all of these have in common is the manipulation of some of the world’s physical properties to achieve some different, material end.
+        </p>
+        <p>
+        A more abstract similarity is the manipulation of information. An assembly line, a series of amplifying transistors, and a chemical process all are instructions for manipulating an input to become an output. A mathematical formula, f(x); an algorithm. Thought processes and routines designed to prepare oneself for a certain environment; techniques for arriving at an answer for a given problem; swinging the hammer down upon the nail. Common to all of these is the idea of a recurring pattern; an innate algorithm to be wielded, with some certain inputs, to solve the issue at hand.
+        </p>
+        <p>
+        Just as a new species emerges in an ecosystem to abuse some untouched resource, a new branch of engineering emerged in the 20th century. Information science–or more practically, computer science. The apex tool, computer science has helped relieve humans of among the most taxing of labors: thinking. Simulations of all kinds, bureaucratic automation, mathematical calculation–all executed in orders of magnitudes less time and calculated with much more precision than a human could ever wish for. The study has more quickly penetrated every discipline of scientific inquiry than any other in history; science without computers has become like mathematics without proof. But like the other disciplines, it hasn’t accomplished this end entirely. Just as physics hasn’t yet solved the problem of utilizing fusion here on earth, computer science hasn’t yet figured out, for example, an algorithm for rectifying the dynamic nature of organically developed inputs. 
+        </p>
+        <p>
+        This analogy could soon become outdated, however, with the recent advances in artificial intelligence. Powerful tools that relieve even more of the labors of thinking, these algorithms can solve mazes, play video games, and write essays better than a lot of existing humans. Their development process is analogous to how animals–or us–rear their young: a given animal is equipped from birth with particuar physiological traits and mental acuities that maximize its ability to survive and reproduce in whatever their host environment may be, being guided by different brain chemistries that tell it how successful its being; similarly, an AI model is equipped with particular mathematical functions that maximize its ability to complete any of its given tasks in a particular domain, all while being guided on the right track by a different function–its loss function.
+        </p>
+        <p>
+        This concept of training a brain to complete some particular task isn’t a new concept; we’ve been doing it since day one with our young, in addition to breeding animals for what they might be able to do for us (e.g. food, work, companionship). In the view of tooling, however, this unlocks a new and improved hammer. A hammer for hammers, such that the hammered hammer may become fit for any type of nail, screw, or otherwise useful bit of hardware. Humanity is very clearly in the process of discovering how to create the most generalized tools in history. Whether or not artificial intelligence will solve the ecosystem of tooling is unclear, but what is certain is that it will become an apex tool and is currently laying the foundations for the following ages of humanity’s toolbox. 
+        </p>
+        <p>
+        From this comes a natural question to ask: how will humanity change in response to the new environment? Environments are never stagnant, and the introduction of a new member only accelerates the change. Accordingly, I expect artificial intelligence to stomp on the gas pedal of cultural change. Nations which wield the new tool(s, as AI most certainly isn’t the only new player) most effectively will become the powerhouses on which the future will depend. America’s isolation during WWII and the corresponding technological developments provided it the foundation to become the leading scientific nation of the latter part of the 20th century. From here, it became a technological powerhouse that currently hosts the most valuable and influential companies on earth. Similarly, the most adoptive nations toward the latest and greatest tooling will become the most productive and progressive in the 21st century and beyond. 
+        </p>
+        <p>
+        The details of the adoption will depend on the continued development of AI. At its current rate, I think it will revolutionize knowledge work. The relative skill floor will lower considerably, as access to humanity's knowledge will become more widespread than ever before. Why? AI could be trained, for example, to answer queries in a given–or any–domain. This is already happening with ChatGPT and GitHub Copilot. This will considerably accelerate the pace of innovation and productivity in every knowledge-based industry. Grunt workers will go from filling out or creating singular spreadsheets to aggregating years of work weekly. The information throughput will skyrocket due to the accessibility of information provided by these new AI.
+        </p>
+        <p>
+        The width of the accessibility channel for this new flood of information will also widen considerably. The past 30 years have been the Age of the Nerd because of their ability to speak the computer’s language to get and manipulate the information required to get an edge on their business opponents. This is about to change with AI’s ability to translate the human language to one a computer easily understands. All sorts of new people will be brought to prominence because of their ability to conglomerate information in new ways that others appreciate. In other words, a heightened premium will be placed on creativity. Lowering the bar on information and tooling will increase value on human-specific skills, and none are more human than the ingenuity of man.
+        </p>
+        <p>
+        The advent of artificial intelligence is going to broaden the dimensionality of the human ecosystem by magnitudes. Concepts of all shapes and sizes that could previously only ever exist in our heads will come to life in ways we can’t even currently imagine. The creativity of humanity is going to blossom in every way imaginable as this new tool provides to us a resource previously gate-kept by the hurdles of mathematical abstraction. Lower the barrier for entry and life will move in to fill as many niches as possible.
+        </p>
+    </div>
+</template>
+
+<style scoped>
+.description {
+    font-size: 1rem;
+    margin-top: 1rem;
+    width: 45rem;
+}
+
+p {
+    text-indent: 2.5em;
+    margin: 1em 0 0 0;
+}
+
+@media (min-width: 1024px) {
+    .project h1,
+    .project h3 {
+        text-align: left;
+    }
+}
+</style>
